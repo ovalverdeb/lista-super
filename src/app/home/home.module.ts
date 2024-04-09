74b8cfc98ea75d2b54de2c158111db265d763266
17203 <17203@portalcedi.edu.mx>
2024-04-09 09:22:23 -0600

    super

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,10 @@
+import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
-
 
 @NgModule({
   imports: [
@@ -14,6 +13,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+declarations: [HomePage]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  shoppingList: { name: string; checked: boolean }[] = [];
+  newItem: string = '';
 
+  addItem() {
+    if (this.newItem.trim() !== '') {
+      this.shoppingList.push({ name: this.newItem, checked: false });
+      this.newItem = ''; // Limpiar el campo después de agregar
+    }
+  }
+
+  toggleItem(item: { name: string; checked: boolean }) {
+    item.checked = !item.checked;
+  }
   constructor() {}
 
 }
